5192ab8ef597daa089027a81da7ac50e5b68bc8d
inoccard <inocenciocardoso19@gmail.com>
2018-12-23 12:33:33 -0200

    User Factory
    configurei o define para produto

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,8 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use App\User;
+use App\Models\Product;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -14,11 +15,20 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
+    
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(\App\Models\Product::class, function (Faker $faker) {
+    
+    return [
+        'nome' => $faker->word,
+        'descricao' => $faker->sentence(),
     ];
 });

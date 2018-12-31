8863397166e4b045db053c4a9e840ae4c46c4511
inoccard <inocenciocardoso19@gmail.com>
2018-12-31 18:08:04 -0200

    Produto Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['nome','descricao'];
+
+    public function rules($id = '')
+    {
+    	return [
+    		'nome' => "required|min:3|max:100|unique:products,nome,{$id},id",
+    		'descricao' => 'required|min:3|max:1500'
+    	];
+    }
+
+    public function rulesSearch()
+    {
+    	return [
+    		'key-search' => 'required',
+    	]; 
+    }
+
+    public function search($data,$totalPage)
+    {
+    	return $this->where('nome',$data['key-search'])->orWhere('descricao','LIKE',"%{$data['key-search']}%")->paginate($totalPage);
+    }
 }

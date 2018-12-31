4e20e22bfdf8ae203877af349f063b5472ad8598
inoccard <inocenciocardoso19@gmail.com>
2018-12-31 18:12:39 -0200

    routas api

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,14 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// API do site
+$this->group(['prefix' => 'V1'], function(){
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	$this->post('auth', 'Auth\AuthApiController@authenticate');
+	$this->post('auth-refresh','Auth\AuthApiController@refreshToken');
+
+	$this->group(['middleware' => 'jwt.auth'], function(){
+		$this->post('products/search','Api\V1\ProductController@search');
+		$this->resource('products','Api\V1\ProductController', ['except' => ['create', 'edit']]);
+	});
 });
-
-$this->resource('products','Api\ProductController', ['except' => ['create', 'edit'
-]]);

7407f0a582958d9af0394a143530d219dca8b673
inoccard <inocenciocardoso19@gmail.com>
2019-01-05 00:29:38 -0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ $this->group(['prefix' => 'V1'], function(){
 	$this->post('auth-refresh','Auth\AuthApiController@refreshToken');
 
 	$this->group(['middleware' => 'jwt.auth'], function(){
-		$this->post('products/search','Api\V1\ProductController@search');
+		$this->get('products/search','Api\V1\ProductController@search');
 		$this->resource('products','Api\V1\ProductController', ['except' => ['create', 'edit']]);
 	});
 });
